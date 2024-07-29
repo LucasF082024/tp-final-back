7057d5041f755d9913243cf01be7706a6e1cef61
Lucas F <capacitaciones06.08@adviters.com>
2024-07-29 14:17:26 -0300

    [ADD] : resources created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { MoviesModule } from './movies/movies.module';
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    ReviewsModule,
+    MoviesModule,
+    GenresModule,
+  ],
   controllers: [],
   providers: [],
 })
